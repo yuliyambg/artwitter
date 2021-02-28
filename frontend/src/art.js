a3de84e675ebc57ca0ab081150be45e1eeb2c899
yuliyambg <yuliyambg@gmail.com>
2021-02-27 20:09:27 -0500

    creating js class art and js class comment

---
+++ frontend/src/art.js
@@ -0,0 +1,8 @@
+class Art{
+  constructor(title, artist_name, image_url){
+    this.title = title;
+    this.artist_name = artist_name;
+    this.image_url = image_url;
+  }
+
+}
---
+++ frontend/src/comment.js
@@ -0,0 +1,7 @@
+class Comment{
+  constructor(content, name, art_id) {
+    this.content = content;
+    this.name = name;
+    this.art_id = art_id;
+  }
+}
