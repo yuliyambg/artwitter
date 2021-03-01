3c381fe82c1fe00656f8d4e9a099e49b9853066c
yuliyambg <yuliyambg@gmail.com>
2021-02-28 22:12:23 -0500

    add comment to art comments

--- backend/artwitter_api/app/controllers/comments_controller.rb
+++ backend/artwitter_api/app/controllers/comments_controller.rb
@@ -19,7 +19,7 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
 
     if @comment.save
-      render json: @comment, status: :created, location: @comment
+      render json: @comment, status: :created
     else
       render json: @comment.errors, status: :unprocessable_entity
     end
@@ -47,11 +47,12 @@ class CommentsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def comment_params
-      params.require(:comment).permit(:text_field, :art_id)
+      params.require(:comment).permit(:content, :name, :art_id)
     end
 
     def set_art
-#     byebug
       @art = Art.find(params[:art_id])
     end
+
+
 end
