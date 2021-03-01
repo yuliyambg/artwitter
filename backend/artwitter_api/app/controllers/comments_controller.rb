872623f1d9c607bd257a546cda3384c1ef8b1c84
yuliyambg <yuliyambg@gmail.com>
2021-02-28 21:49:09 -0500

    fix comments index controller

--- backend/artwitter_api/app/controllers/comments_controller.rb
+++ backend/artwitter_api/app/controllers/comments_controller.rb
@@ -1,9 +1,10 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :update, :destroy]
+  before_action :set_art, only: [:index, :show, :update, :destroy]
 
   # GET /comments
   def index
-    @comments = Comment.all
+    @comments = @art.comments
 
     render json: @comments
   end
@@ -47,5 +48,10 @@ class CommentsController < ApplicationController
     # Only allow a trusted parameter "white list" through.
     def comment_params
       params.require(:comment).permit(:text_field, :art_id)
+    end
+
+    def set_art
+#     byebug
+      @art = Art.find(params[:art_id])
     end
 end
